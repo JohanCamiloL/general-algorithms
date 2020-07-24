355e0ad95597997a38b9e644a6e934fb342352e3
Camilo Lopez <johan.lopez@bluetab.net>
2020-07-24 18:38:47 -0500

    ES6 implementation deleted

--- linear-search/node/App.js
+++ linear-search/node/App.js
@@ -1,7 +1,6 @@
 const linearSearch = require('./LinearSearch')
 
 const array = [4, 5, 6, 7, 3, 4, 6];
-const number = 3;
+const number = 4;
 
 console.log(linearSearch.linearSearch(array, number));
-console.log(linearSearch.linearSearchWithES6(array, number));
--- linear-search/node/LinearSearch.js
+++ linear-search/node/LinearSearch.js
@@ -14,21 +14,4 @@ const linearSearch = (array, number) => {
     return -1;
 }
 
-/**
- * Linear search algorithm implementation with ES6.
- * @param {Array} array Array of numbers.
- * @param {Number} number Number to be searched.
- * @returns {Number} Number index, if not found returns -1.
- */
-const linearSearchWithES6 = (array, number) => {
-    return array.findIndex(num => num === number);
-
-    array.forEach((num, index) => { if (num === number) return index });
-
-    return -1;
-}
-
-module.exports = {
-    linearSearch,
-    linearSearchWithES6
-};
+module.exports = { linearSearch };
